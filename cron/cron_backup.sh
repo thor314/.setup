c4a949d66c384609d8ba0e719b328427fdf76499
thor <thorck@protonmail.com>
2022-11-06 11:39:01 -0800

    bo-orb-2022-11-06 19:39UTC

--- cron/cron_backup.sh
+++ cron/cron_backup.sh
@@ -5,7 +5,7 @@
 echo "cronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)" >> /home/thor/.setup/cron/cronlog
 
 cd /home/thor/.setup
-ssh-add ~/.ssh/id_ed25519
+eval `ssh-agent -s`
 git add --all && git commit -m "$(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)" 
 git pull 
 git push
