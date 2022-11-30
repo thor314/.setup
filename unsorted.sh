a8961783e1f8be539eaa835baccd587d671ec2c1
thor <thorck@protonmail.com>
2022-11-30 08:30:01 -0800

    pop-os-2022-11-30 16:30UTC

--- unsorted.sh
+++ unsorted.sh
@@ -7,3 +7,6 @@ wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
 agi gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 #deps
 sudo dpkg -i protonvpn-stable-release_1.0.1-1_all.deb
 agi protonvpn-cli && popd
+
+# bu dumps
+agi -y fish
