6b830ef4ee01486a65542117914617e55673dfe1
thor <thorck@protonmail.com>
2022-11-30 11:50:01 -0800

    pop-os-2022-11-30 19:50UTC

--- unsorted.sh
+++ unsorted.sh
@@ -9,5 +9,9 @@ sudo dpkg -i protonvpn-stable-release_1.0.1-1_all.deb
 agi protonvpn-cli && popd
 
 # bu dumps
-agi -y fish
+# agi -y fish # not up-to-date
+# go here: https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+packages
+# version will likely change. copied fish to .share/bin.
+wget https://launchpad.net/\~fish-shell/+archive/ubuntu/release-3/+files/fish_3.5.1-1\~xenial_amd64.deb
+sudo dpkg -i fish_*.deb
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
