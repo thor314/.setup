26c286f3b627f00b5f4bfa10dd5b0279ad35bfb4
Thor Kamphefner <thorck@pm.me>
2023-03-21 09:10:01 -0700

    boorb-2023-03-21 16:10UTC

--- 08-cron.sh
+++ 08-cron.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# todo
+# set up cronscripts in /var/spool
+echo "calling crontab -e, copy and manually paste the following into it"
+cat $HOME/cron/crontab
+
+echo -e "\n\ndon't forget to include a newline at the end of the crontab"
+read -p "Copy and press enter to continue"
+crontab -e
