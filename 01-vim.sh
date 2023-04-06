4f9fb2b9b596b9fbc2d76996d9577e77451db45e
Thor Kamphefner <thorck@pm.me>
2023-04-06 13:10:01 +0100

    lil-birb-2023-04-06 12:10UTC

--- 01-vim.sh
+++ 01-vim.sh
@@ -4,3 +4,7 @@
 sudo apt install vim-gui-common # fully featured vim
 sudo apt install vim-runtime # fully featured vim
 
+# install helix while we're at it
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
