6e8e3590e7d4ea9ed60aaef898f8149eff7cbb09
Thor Kamphefner <thorck@pm.me>
2023-04-06 13:50:01 +0100

    lil-birb-2023-04-06 12:50UTC

--- 01-vim.sh
+++ 01-vim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # install better vim
-sudo apt install vim-gui-common # fully featured vim
-sudo apt install vim-runtime # fully featured vim
+sudo apt install -y vim-gui-common # fully featured vim
+sudo apt install -y vim-runtime # fully featured vim
 
 # install helix while we're at it
-sudo add-apt-repository ppa:maveonair/helix-editor
-sudo apt update
-sudo apt install helix
+sudo add-apt-repository -y ppa:maveonair/helix-editor  
+sudo apt update -y
+sudo apt install -y helix
