3673ffdb0da82f93d5889b19ad5e33dadce058a3
Thor Kamphefner <thorck@pm.me>
2023-06-11 11:10:01 -0700

    boorb-2023-06-11 18:10UTC

--- unsorted.sh
+++ unsorted.sh
@@ -1,3 +1,5 @@
 # recent commands (as of 2023-03-21)
 pip install chatblade # openai cli tool https://github.com/npiv/chatblade
 flatpak install -y flathub com.slack.Slack
+# Bloop: https://github.com/BloopAI/bloop/releases/tag/v0.4.4
+
