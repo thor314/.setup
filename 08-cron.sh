ec73564da0f743af020518f19e8e27f176e59c5f
Thor Kamphefner <thorck@pm.me>
2023-09-12 13:52:04 -0700

    sudo cron update

--- 08-cron.sh
+++ 08-cron.sh
@@ -10,5 +10,10 @@ echo "calling crontab -e, copy and manually paste the following into it"
 cat $HOME/cron/crontab
 
 echo -e "\n\ndon't forget to include a newline at the end of the crontab"
-read -p "Copy and press enter to continue"
+read -p "press enter to continue"
 crontab -e
+
+echo -e "\n\nput the following in my sudo crontab:"
+echo -e "0 12 * * * apt update && sudo apt upgrade -y"
+read -p "press enter to continue"
+
