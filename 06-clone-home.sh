88a8c0acbf4a0644b2a5cad2faed0234229bf5e4
Thor Kamphefner <thorck@pm.me>
2023-09-11 19:02:37 -0700

    clone home

--- 06-clone-home.sh
+++ 06-clone-home.sh
@@ -6,17 +6,17 @@ rm -r Documents Desktop Music Pictures Public Templates Videos
 
 gh repo clone .private
 gh repo clone .files
-PATH="$PATH:$HOME/.local/bin"
 
+pip install dotbot
+PATH="$PATH:$HOME/.local/bin"
 cd $HOME/.files && dotbot -c install.conf.yaml && cd
 cd $HOME/.private && dotbot -c install.conf.yaml && cd
 
-
 gh repo clone blog 
-# hub clone cryptography
-mkdir img
-gh repo clone backgrounds img/backgrounds
-gh repo clone profile img/profile
+mkdir img && cd img
+gh repo clone backgrounds 
+gh repo clone profile 
+cd 
 gh repo clone keep
 gh repo clone cron 
 popd
