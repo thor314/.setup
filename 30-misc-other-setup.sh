3f0923ac5ca08f44512fb40a0b6ac9747df11e57
Thor Kamphefner <thorck@pm.me>
2023-09-12 11:20:01 -0700

    boorb-2023-09-12 18:20UTC

---
+++ 30-misc-other-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# other things to do
+
+chmod 755 .trash archive fun vid # otherwise they appear uggo green in terminal
+echo "turn off icons in Extensions"
+echo "poke around Settings, update keybinds"
+echo "set up 1Password, Obsidian, Firefox, Spotify, Signal, Telegram, etc."
+
