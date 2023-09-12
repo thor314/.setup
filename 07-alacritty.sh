94c2230f7c090c908928e45c2f5dba3db6226bcf
Thor Kamphefner <thorck@pm.me>
2023-09-11 18:49:05 -0700

    alacritty moved, fisher moved

---
+++ 07-alacritty.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 scdoc
+
+pushd $HOME/.setup/clones
+
+# from https://github.com/alacritty/alacritty/blob/master/INSTALL.md#debianubuntu
+git clone git@github.com:alacritty/alacritty.git
+cd alacritty
+cargo build --release
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+sudo mkdir -p /usr/local/share/man/man5
+scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+
+# ZSH COMPLETIONS
+# mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+# echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+# cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+
+# FISH COMPLETIONS (do this in fish)
+# mkdir -p $fish_complete_path[1]
+# cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
+
+popd
+
