609aa18e82e61d51a349117153fea99d7faf98ad
Thor Kamphefner <thorck@pm.me>
2023-10-16 13:00:04 +0100

    pop-os-2023-10-16 12:00UTC

--- 20-gui-common.sh
+++ 20-gui-common.sh
@@ -6,8 +6,9 @@ sudo apt install -y vlc # simple playback
 sudo apt install -y evince # secondary pdf viewer, after Zotero
 
 # telegram fuckery
+# apt isn't up to date
 # sudo apt install -y telegram-desktop # apt falls out of date a lot
-flatpak install -y flathub org.telegram.desktop # apt isn't up to date
+flatpak install -y flathub org.telegram.desktop 
 
 # I'm learning: use flatpak > snap in general. Tends to be well maintained, low overhead
 flatpak install -y flathub codes.merritt.FeelingFinder # emoji picker
@@ -27,7 +28,6 @@ flatpak install -y flathub org.gimp.GIMP
 # installing code is a mess, every other installation is worse
 echo -e "\n\n install vsCode: https://code.visualstudio.com/Download"
 echo -e "\n\n install edge: https://www.microsoft.com/en-us/edge/download"
-# echo -e "\n\n install 1Password: https://1password.com/downloads/linux/"
 read -p "press enter to continue"
 
 wget https://github.com/federico-terzi/espanso/releases/download/v2.1.8/espanso-debian-x11-amd64.deb
