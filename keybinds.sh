75a55fc0ad4fdb094a83a6bf6c2459efafa5a6fa
Thor Kamphefner <thorck@pm.me>
2023-11-03 11:45:06 +0300

    pop-os-2023-11-03 08:45UTC

--- keybinds.sh
+++ keybinds.sh
@@ -4,79 +4,112 @@
 # Unbind conflicting hotkeys (unsure how), needs debugging
 # gsettings set org.gnome.shell.extensions.pop-shell float-window '[]'
 
+MEDIA_KEYS=org.gnome.settings-daemon.plugins.media-keys.custom-keybinding
+BIND_DIR=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/
+
 # Register space for plenty of custom keybindings
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/', '/org/gnome/settings-daemon/plugins.media-keys/custom-keybindings/custom4/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom10/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom11/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom12/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom13/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom14/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom15/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom16/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom17/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom18/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom19/' ]"
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/', 
+  '/org/gnome/settings-daemon/plugins.media-keys/custom-keybindings/custom4/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom10/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom11/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom12/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom13/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom14/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom15/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom16/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom17/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom18/', 
+  '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom19/' ]"
 
 # tdrop hotkeys
 # Alacritty (Super+G)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Alacritty tdrop'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'tdrop -ma -h 100% -w 50% alacritty'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Super+G'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom0/ name 'Alacritty tdrop'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom0/ command 'tdrop -ma -h 100% -w 50% alacritty'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom0/ binding 'Super+G'
 
 # Telegram (Super+T)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Telegram'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'tdrop -ma -h 100% -w 50% -n telegram --class=telegram flatpak run org.telegram.desktop'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding 'Super+T'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom1/ name 'Telegram'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom1/ command 'tdrop -ma -h 100% -w 50% -n telegram --class=telegram flatpak run org.telegram.desktop'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom1/ binding 'Super+T'
 
 # Signal (Super+S)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'Signal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command 'tdrop -ma -h 100% -w 50% -n signal --class=signal flatpak run org.signal.Signal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding 'Super+S'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom2/ name 'Signal'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom2/ command 'tdrop -ma -h 100% -w 50% -n signal --class=signal flatpak run org.signal.Signal'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom2/ binding 'Super+S'
 
 # Discord (Super+D)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ name 'Discord'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ command 'tdrop -ma -h 100% -w 50% -n discord --class=discord flatpak run com.discordapp.Discord'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ binding 'Super+D'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom3/ name 'Discord'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom3/ command 'tdrop -ma -h 100% -w 50% -n discord --class=discord flatpak run com.discordapp.Discord'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom3/ binding 'Super+D'
 
 # Obsidian (Super+O)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ name 'Obsidian'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ command 'tdrop -ma -h 100% -w 50% -n obsidian --class=obsidian flatpak run md.obsidian.Obsidian'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ binding 'Super+O'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom4/ name 'Obsidian'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom4/ command 'tdrop -ma -h 100% -w 50% -n obsidian --class=obsidian flatpak run md.obsidian.Obsidian'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom4/ binding 'Super+O'
 
 # Firefox (Super+F)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/ name 'Firefox'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/ command 'tdrop -ma -h 100% -w 50% firefox'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/ binding 'Super+F'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom5/ name 'Firefox'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom5/ command 'tdrop -ma -h 100% -w 50% firefox'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom5/ binding 'Super+F'
 
 # Feeling Finder (Super+E)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/ name 'Feeling Finder'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/ command 'tdrop -ma -h 100% -w 50% -n feelingfinder --class=feelingfinder flatpak run codes.merritt.FeelingFinder'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/ binding 'Super+E'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom6/ name 'Feeling Finder'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom6/ command 'tdrop -ma -h 100% -w 50% -n feelingfinder --class=feelingfinder flatpak run codes.merritt.FeelingFinder'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom6/ binding 'Super+E'
 
 # Spotify (Super+Shift+S)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom11/ name 'Spotify' 
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom11/ command 'tdrop -ma -h 100% -w 50% -n spotify --class=spotify flatpak run com.spotify.Client' 
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom11/ binding 'Super+Shift+S'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom11/ name 'Spotify' 
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom11/ command 'tdrop -ma -h 100% -w 50% -n spotify --class=spotify flatpak run com.spotify.Client' 
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom11/ binding 'Super+Shift+S'
 
 # non-tdrop hotkeys
 # Open Firefox with multiple tabs (Super+B)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom10/ name 'Firefox Tabs'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom10/ command 'firefox --new-tab "https://www.coingecko.com/en/portfolio" --new-tab "https://eprint.iacr.org/days/7" --new-tab "https://feedly.com/" --new-tab "https://news.ycombinator.com/front" --new-tab "https://tweetdeck.twitter.com/" --new-tab "https://zkmesh.substack.com/" --new-tab "https://github.com/thor314"'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom10/ binding 'Super+B'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom10/ name 'Firefox Tabs'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom10/ command 'firefox --new-tab "https://www.coingecko.com/en/portfolio" --new-tab "https://eprint.iacr.org/days/7" --new-tab "https://feedly.com/" --new-tab "https://news.ycombinator.com/front" --new-tab "https://tweetdeck.twitter.com/" --new-tab "https://zkmesh.substack.com/" --new-tab "https://github.com/thor314"'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom10/ binding 'Super+B'
+
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom14/ name 'Firefox Pinned'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom14/ command 'firefox -new-tab "https://linear.app/th0r/my-issues/assigned" \
+-new-tab "https://claude.ai/chats" \
+-new-tab "https://chat.openai.com/" \
+-new-tab "https://assistant.google.com/tasks?otzr=1" \
+-new-tab "https://calendar.google.com/calendar/u/0/r/week" \
+-new-tab "https://mail.proton.me/u/0/inbox"'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom14/ binding 'Super+Shift+P'
 
 # Normal hotkeys
 # Alacritty (Super+T)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/ name 'Alacritty'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/ command 'alacritty'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/ binding 'Super+t'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom7/ name 'Alacritty'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom7/ command 'alacritty'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom7/ binding 'Super+t'
 
 # Zotero (Super+Z)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/ name 'Zotero'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/ command 'flatpak run org.zotero.Zotero'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/ binding 'Super+Z'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom8/ name 'Zotero'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom8/ command 'flatpak run org.zotero.Zotero'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom8/ binding 'Super+Z'
 
 # Code (Super+C)
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/ name 'Code'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/ command 'code'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/ binding 'Super+C'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom9/ name 'Code'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom9/ command 'code'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom9/ binding 'Super+C'
 
 
 # Keyboard layout switching
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom12/ name 'Switch to Dvorak'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom12/ command 'setxkbmap dvorak -option ctrl:nocaps'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom12/ binding 'Menu'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom12/ name 'Switch to Dvorak'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom12/ command 'setxkbmap dvorak -option ctrl:nocaps'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom12/ binding 'Menu'
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom13/ name 'Switch to US'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom13/ command 'setxkbmap dvorak -option ctrl:nocaps'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom13/ binding 'Shift+Menu'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom13/ name 'Switch to US'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom13/ command 'setxkbmap us -option ctrl:nocaps'
+gsettings set $MEDIA_KEYS:$KEYBIND_DIR/custom13/ binding 'Shift+Menu'
+
 
