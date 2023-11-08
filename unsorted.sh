3d3abd75488805c645b571699231a32afcc2c616
Thor Kamphefner <thorck@pm.me>
2023-11-08 20:50:17 +0300

    pop-os-2023-11-08 17:50UTC

--- unsorted.sh
+++ unsorted.sh
@@ -6,6 +6,6 @@ flatpak install flathub com.valvesoftware.Steam
 # https://guide.penumbra.zone/main/pcli/install.html#linux
 break
 sudo apt install build-essential pkg-config libssl-dev clang git-lfs -y
-git clone https://github.com/penumbra-zone/penumbra
-#cd penumbra && git fetch && git checkout v0.63.1
+git clone https://github.com/penumbra-zone/penumbra clones
+#cd clones/penumbra && git fetch && git checkout v0.63.1
 #cargo build --release --bin pcli
