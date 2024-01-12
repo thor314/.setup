21e0695d046adea0a18b31c057ec723265fb35e7
Thor Kamphefner <thorck@pm.me>
2024-01-12 14:12:30 -0800

    birb-2024-01-12 22:12UTC

--- 06-clone-home.sh
+++ 06-clone-home.sh
@@ -12,21 +12,21 @@ PATH="$PATH:$HOME/.local/bin"
 cd $HOME/.files && dotbot -c install.conf.yaml && cd
 cd $HOME/.private && dotbot -c install.conf.yaml && cd
 
-gh repo clone blog 
-mkdir img && cd img
-gh repo clone backgrounds 
-gh repo clone profile 
-gh repo clone official-images
-cd 
 gh repo clone .keep
 gh repo clone .cron 
 
-gh repo clone official
-gh repo clone invoice
-gh repo clone obsidian-setup
+gh repo clone projects
 
-mkdir r && cd r
-gh repo clone tmpl
-cd
+mkdir r 
+mkdir fun
 
 popd
+
+# mkdir img && cd img
+# gh repo clone backgrounds 
+# gh repo clone profile 
+# gh repo clone official-images
+# cd 
+
+# gh repo clone official
+# gh repo clone invoice
