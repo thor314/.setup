c0ab2c0c7c02a0c78ef1dd7e61c2984fd7769521
Thor Kampefner <thorck@pm.me>
2024-10-09 20:52:51 -0700

    deprecate some things in node

--- modules/02-home.sh
+++ modules/02-home.sh
@@ -9,6 +9,8 @@ for repo in "${repos[@]}"; do
 	gh repo clone $repo
 done
 
+gh repo clone helix $HOME/.config/helix
+
 pip install dotbot
 PATH="$PATH:$HOME/.local/bin"
 cd $HOME/.files && dotbot -c install.conf.yaml && cd
--- modules/13-node.sh
+++ modules/13-node.sh
@@ -7,12 +7,10 @@ npm i -g pnpm
 pnpm i -g vite
 
 pnpm i -g create-react-app
-pnpm i -D -g node-sass @types/node-sass
 
 pnpm i -g bash-language-server # https://github.com/bash-lsp/bash-language-server
 # https://github.com/mermaid-js/mermaid-cli
 pnpm i -g @mermaid-js/mermaid-cli && ./node_modules/.bin/mmdc -h
-
 
 ## Install misc less-used but maybe useful language servers 
 # https://github.com/helix-editor/helix/wiki/How-to-install-the-default-language-servers
@@ -22,16 +20,18 @@ pnpm i -g typescript-language-server typescript # https://github.com/typescript-
 pnpm i -g dockerfile-language-server-nodejs # https://github.com/rcjsuen/dockerfile-language-server-nodejs
 pnpm i -g vscode-langservers-extracted # vscode's HTML/JSON/CSS:  https://github.com/hrsh7th/vscode-langservers-extracted
 
-# lean prover
-# https://leanprover-community.github.io/install/debian.html
-wget -q https://raw.githubusercontent.com/leanprover-community/mathlib-tools/master/scripts/install_debian.sh && bash install_debian.sh ; source ~/.profile
-rm install_debian.sh
-pnpm i -g lean-language-server
-
-# Issues:  solc not detected by helix, may be upstream issues *shrug*, not my monkeys
-# npm i -g solc # https://docs.soliditylang.org/en/latest/installing-solidity.html
-
 # bun https://bun.sh/
 curl -fsSL https://bun.sh/install | bash
 npm install -g typescript-language-server typescript
 
+# deprecated
+# lean prover
+# https://leanprover-community.github.io/install/debian.html
+# wget -q https://raw.githubusercontent.com/leanprover-community/mathlib-tools/master/scripts/install_debian.sh && bash install_debian.sh ; source ~/.profile
+# rm install_debian.sh
+# pnpm i -g lean-language-server
+
+# Issues:  solc not detected by helix, may be upstream issues *shrug*, not my monkeys
+# npm i -g solc # https://docs.soliditylang.org/en/latest/installing-solidity.html
+
+# pnpm i -D -g node-sass @types/node-sass # deprecated
