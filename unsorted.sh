6e3073e9585fd83796b4c12a97b2377247ac80b5
Thor Kampefner <thorck@pm.me>
2024-10-10 15:23:18 -0700

    notes on cosmic de

--- unsorted.sh
+++ unsorted.sh
@@ -10,3 +10,12 @@ hub clone --recursive web-playground
 # protonmail gui deprecated until they fix their notification bug
 pushd debs && wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.deb && sudo dpkg -i ProtonMail*.deb && popd 
 
+# ossify to 00-init.sh 
+## Cosmic DE installation 
+# enable wayland and adjust DE run settings
+sudo sed -i 's/^#\?WaylandEnable=.*$/WaylandEnable=true/' /etc/gdm3/custom.conf
+sed -i '/LABEL="gdm_prefer_xorg"/,/LABEL="gdm_end"/ s/^RUN+=/# &/' /usr/lib/udev/rules.d/61-gdm.rules
+sudo apt install -y cosmic-session
+read -p "the following will logout. Reboot, and login back in with Cosmic DE. Press enter."
+sudo systemctl restart gdm
+
