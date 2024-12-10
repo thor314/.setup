2813c2739b42d95a221b35f3c4426b4a72686713
Thor Kampefner <thorck@pm.me>
2024-12-10 14:05:16 -0700

    gui setups

--- modules/14-gui.sh
+++ modules/14-gui.sh
@@ -10,7 +10,7 @@ sudo apt install -y evince # secondary pdf viewer, after Zotero
 flatpak install -y flathub org.chromium.Chromium
 flatpak install -y flathub md.obsidian.Obsidian
 flatpak install -y flathub us.zoom.Zoom  
-flatpak install -y flathub org.zotero.Zotero 
+flatpak install -y flathub org.zotero.Zotero
 flatpak install -y flathub com.github.eneshecan.WhatsAppForLinux
 flatpak install -y flathub org.signal.Signal
 flatpak install -y flathub com.discordapp.Discord
@@ -21,6 +21,11 @@ flatpak install -y flathub com.slack.Slack
 flatpak install -y flathub codes.merritt.FeelingFinder # emoji picker
 # flatpak install -y flathub com.obsproject.Studio
 # flatpak install -y flathub org.gimp.GIMP
+
+# https://www.spotify.com/us/download/linux/
+# unclear whether flatpak spotify or deb has more more gui issues 
+flatpak install -y flathub com.spotify.Client
+
 echo "go to telegram settings: advanced: turn off tray icon (and OpenGL if rendering issues), and all notifications"
 
 # installing code is a mess, every other installation is worse
@@ -28,9 +33,11 @@ echo -e "\n\n install vsCode: https://code.visualstudio.com/Download"
 echo -e "\n\n install android messages: https://github.com/OrangeDrangon/android-messages-desktop/releases/tag/v5.4.2" 
 read -p "press enter to continue"
 
-# spotify backup
-# https://www.spotify.com/us/download/linux/
-flatpak install -y flathub com.spotify.Client # flatpak spotify more gui issues than bin, +tdrop issues, see below
+
+###########
+# ARCHIVE #
+###########
+# old spotify installation
 # curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 # echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 # Then you can install the Spotify client:
@@ -39,7 +46,13 @@ flatpak install -y flathub com.spotify.Client # flatpak spotify more gui issues 
 # firefox --new-tab https://protonvpn.com/support/official-linux-vpn-debian/
 # read -p "download the latest protonvpn cli, following the instructions above"
 
-# ARCHIVE
+# # zotero install; tried this when flatpak zotero wouldn't obey tdrop
+# # https://www.zotero.org/support/installation
+# # https://github.com/retorquere/zotero-deb?tab=readme-ov-file
+# wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
+# sudo apt update
+# sudo apt install zotero
+
 # echo -e "\n\n install edge: https://www.microsoft.com/en-us/edge/download"
 
 ## Telegram installation notes
